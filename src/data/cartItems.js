c3bee90de8326f6e4f166a406f0349e8046f411d
wilder0808 <wilder0808@gmail.com>
2025-05-09 10:47:03 -0500

    Se crea listado de items que se mostrarán en resumen para la compra de los libros

---
+++ src/data/cartItems.js
@@ -0,0 +1,26 @@
+export const cartItems = [
+  {
+    id: 1,
+    title: "El Señor de los Anillos",
+    author: "J.R.R. Tolkien",
+    price: 29.99,
+    type: "físico",
+    quantity: 2
+  },
+  {
+    id: 2,
+    title: "1984",
+    author: "George Orwell",
+    price: 19.99,
+    type: "digital",
+    quantity: 1
+  },
+  {
+    id: 3,
+    title: "Cien años de soledad",
+    author: "Gabriel García Márquez",
+    price: 24.99,
+    type: "físico",
+    quantity: 1
+  }
+]; 
